4f431cd1f9aee1b2abadd99c4871809ceac6ecae
Amir Salehi <nik.amirsa@gmail.com>
2022-08-20 16:03:38 +0430

    create gitattributes file

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,11 @@
+import { Button } from "antd"
+
 export default function About() {
     return (
         <div>
             <h1>About</h1>
             <p>This is about page</p>
+            <Button>Back to home</Button>
         </div>
     )
 }
