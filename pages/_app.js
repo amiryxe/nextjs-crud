ffe6bd79cd6e3dea03e65564a9a8a1b97f177fe3
Amir Salehi <nik.amirsa@gmail.com>
2022-08-21 16:12:40 +0430

    add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,16 @@
 import '../styles/globals.css'
-import 'antd/dist/antd.variable.min.css'
 // import 'antd/dist/antd.dark.css';
+import 'antd/dist/antd.variable.min.css'
+import '../styles/lib/__index.scss'
+// import '../styles/lib/_dark.scss'
 // import 'antd/dist/antd.compact.css';
 
-import LayoutWrapper from '../components/Layout'
 import { ConfigProvider } from 'antd'
+import LayoutWrapper from '../components/Layout'
 
 ConfigProvider.config({
   theme: {
-    primaryColor: '#25b864',
-    backgroundColor: '#333',
-    backgroundColorBase: '#333',
-    backgroundColorLight: '#333',
-    backgroundColorDark: '#333',
-    backgroundColorDanger: '#333',
-    backgroundColorInfo: '#333',
-    backgroundColorSuccess: '#333',
-    backgroundColorWarning: '#333',
-
-
+    primaryColor: 'darkorange',
   },
 });
 
