fe5c042d649e06330eea881993bfc14a24711fbc
Amir Salehi <nik.amirsa@gmail.com>
2023-09-06 15:46:43 +0330

    create instance of prisma client

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,5 @@
+import prisma from "@/utils/connect"
 import { NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
 
 export const GET = async () => {
     try {
