b0bbe393959c8e1940053a808564b45eed777ec1
Amir Salehi <nik.amirsa@gmail.com>
2023-09-08 14:27:52 +0330

    create order page and handle error

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -3,20 +3,25 @@
 import { useQuery } from '@tanstack/react-query'
 
 export default function Orders() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, status, error } = useQuery({
     queryKey: ['orders'],
     queryFn: async () => {
-      const res = await fetch('http://localhost:3000/api/orders')
-      const data = await res.json()
-      return data
+      fetch('http://localhost:3000/api/orders').then((res) => res.json())
     },
   })
 
   console.log(data)
+  console.log(error)
 
   return (
     <div>
       <h1>List of Orders</h1>
+
+      {status === 'error' && (
+        <p className="bg-red-200 border text-red-950 border-red-400 p-4 m-4 rounded-lg">
+          You don't have access
+        </p>
+      )}
     </div>
   )
 }
